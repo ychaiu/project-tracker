67c032fa32f49dc46674185700e19a90d31a2844
Hackbright Student <no-reply@hackbrightacademy.com>
2018-10-27 12:10:48 -0700

    finish project tracker

--- hackbright.sql
+++ hackbright.sql
@@ -33,6 +33,41 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
+-- Name: grades; Type: TABLE; Schema: public; Owner: user
+--
+
+CREATE TABLE public.grades (
+    id integer NOT NULL,
+    student_github character varying(30),
+    project_title character varying(30),
+    grade integer
+);
+
+
+ALTER TABLE public.grades OWNER TO "user";
+
+--
+-- Name: grades_id_seq; Type: SEQUENCE; Schema: public; Owner: user
+--
+
+CREATE SEQUENCE public.grades_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.grades_id_seq OWNER TO "user";
+
+--
+-- Name: grades_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: user
+--
+
+ALTER SEQUENCE public.grades_id_seq OWNED BY public.grades.id;
+
+
+--
 -- Name: projects; Type: TABLE; Schema: public; Owner: user
 --
 
@@ -106,6 +141,13 @@ ALTER SEQUENCE public.students_id_seq OWNED BY public.students.id;
 -- Name: id; Type: DEFAULT; Schema: public; Owner: user
 --
 
+ALTER TABLE ONLY public.grades ALTER COLUMN id SET DEFAULT nextval('public.grades_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: user
+--
+
 ALTER TABLE ONLY public.projects ALTER COLUMN id SET DEFAULT nextval('public.projects_id_seq'::regclass);
 
 
@@ -117,12 +159,32 @@ ALTER TABLE ONLY public.students ALTER COLUMN id SET DEFAULT nextval('public.stu
 
 
 --
+-- Data for Name: grades; Type: TABLE DATA; Schema: public; Owner: user
+--
+
+COPY public.grades (id, student_github, project_title, grade) FROM stdin;
+1	jhacks	Markov	10
+2	jhacks	Blockly	2
+3	sdevelops	Markov	50
+4	sdevelops	Blockly	100
+\.
+
+
+--
+-- Name: grades_id_seq; Type: SEQUENCE SET; Schema: public; Owner: user
+--
+
+SELECT pg_catalog.setval('public.grades_id_seq', 4, true);
+
+
+--
 -- Data for Name: projects; Type: TABLE DATA; Schema: public; Owner: user
 --
 
 COPY public.projects (id, title, description, max_grade) FROM stdin;
 1	Markov	Tweets generated from Markov chains	50
 2	Blockly	Programmatic Logic Puzzle Game	100
+3	Project Tracker	DB YADADA	10
 \.
 
 
@@ -130,7 +192,7 @@ COPY public.projects (id, title, description, max_grade) FROM stdin;
 -- Name: projects_id_seq; Type: SEQUENCE SET; Schema: public; Owner: user
 --
 
-SELECT pg_catalog.setval('public.projects_id_seq', 2, true);
+SELECT pg_catalog.setval('public.projects_id_seq', 3, true);
 
 
 --
@@ -148,6 +210,14 @@ COPY public.students (id, first_name, last_name, github) FROM stdin;
 --
 
 SELECT pg_catalog.setval('public.students_id_seq', 2, true);
+
+
+--
+-- Name: grades_pkey; Type: CONSTRAINT; Schema: public; Owner: user
+--
+
+ALTER TABLE ONLY public.grades
+    ADD CONSTRAINT grades_pkey PRIMARY KEY (id);
 
 
 --
